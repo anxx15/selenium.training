15462c992d16bc8d2e44af287770c974f36fd171
anupp <anupp@192.168.0.15>
2019-10-21 21:49:03 -0400

    added new classes

--- src/test/java/utils/CommonMethod.java
+++ src/test/java/utils/CommonMethod.java
@@ -1,12 +1,16 @@
 package utils;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonMethod {
 	
+	private static final long DEFAULT_WAIT = 100;
 	protected WebDriver driver;
+	 protected WebDriverWait smartWait;
 	
 	public CommonMethod(WebDriver driver) {
 		this.driver = driver;
+		this.smartWait = new WebDriverWait(driver, DEFAULT_WAIT);
 	}
 }
